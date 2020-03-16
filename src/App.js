d1a1fb7f05b75cfcff7c4e9e9707861368ab110f
poushitaguha <pguha.kolkata@gmail.com>
2020-03-16 18:42:29 -0400

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component, Fragment } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <h1>Hello from React</h1>
+        <Navbar />
       </Fragment>
     );
   }
